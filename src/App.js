82643610a6c4a20932df16a14768ae7442295b18
Syed Rizwan Ahmed <shah@controller.Home>
2021-08-01 12:45:07 +0100

    V3 commit index.html

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Kuberenetes Cluster
+          Hello Kuberenetes Cluster/Change version to V3
         </p>
         <a
           className="App-link"
